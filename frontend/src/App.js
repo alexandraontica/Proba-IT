e566fb85c770d4d680d9edd7258dc7abb0952c34
Alexandra Onțică <150956540+alexandraontica@users.noreply.github.com>
2023-12-06 00:14:08 +0200

    get request + front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,21 @@ import BasicNavbar from './Navbar/BasicNavbar.js';
 import background from './background.png'
 import Footer from './Footer/Footer.js';
 import Poll from './Poll/Poll.js';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+const [pollsData, setPollsData] = useState([]);
+
+useEffect(() => {
+  const getData = async() => {
+    const response = await axios.get('http://localhost:5000/api/poll');
+    setPollsData(response.data.polls);
+  }
+
+  getData();
+}, []);
+
   return (
     <div className="App" style={{backgroundImage: `(${background})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
       <BasicNavbar/>
@@ -15,23 +28,18 @@ function App() {
           <p className="paragraph">Opiniile sunt mai importante ca niciodată. Platformele de sondaje permit organizatorilor să culeagă feedback direct de la audiența lor și să înțeleagă mai bine nevoile și dorințele acesteia.</p>
           <img src={turtle} className="App-logo myLogo" alt="logo" />
         </div>
-        <div className='d-flex flex-row justify-content-evenly w-100' >
-        <Poll
-          question="What's your favorite color?"
-          options={[
-            { id: 1, text: 'Red' },
-            { id: 2, text: 'Blue' },
-            { id: 3, text: 'Green' },
-          ]}
-        />
-        <Poll
-          question="What's your favorite color?"
-          options={[
-            { id: 1, text: 'Red' },
-            { id: 2, text: 'Blue' },
-            { id: 3, text: 'Green' },
-          ]}
-        />
+        <div className='d-flex flex-wrap justify-content-evenly w-100' >
+        {pollsData.map((poll) => (
+            <Poll
+             key={poll._id} className="poll-wrapper mb-4"
+              question={poll.question}
+              options={[
+                { id: 1, text: poll.option1 },
+                { id: 2, text: poll.option2 },
+                { id: 3, text: poll.option3 },
+              ]}
+            />
+          ))}
         </div>
         </header>
         <Footer/>
